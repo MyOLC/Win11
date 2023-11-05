c7f3e61dbc98a12394087a8ac54f7f63ea37b311
MyOLC <115734188+MyOLC@users.noreply.github.com>
2023-11-05 13:02:31 +0000

    Create OOBEManual.ps1

---
+++ OOBEManual.ps1
@@ -0,0 +1,2 @@
+Start /Wait PowerShell -C Set-ExecutionPolicy Unrestricted; Install-Module OSD -Force ; Import-Module OSD -Force
+Start-OSDPad -RepoOwner MyOLC -RepoName MyScripts -BrandingTitle 'Our Learning Cloud - Bhavin Patel' -OAuth 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 -hide script -BrandingColor Magenta
