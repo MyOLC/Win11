6e01848a0f9f89e1a01b4b3a8e636107569014db
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-05 20:40:19 +0000

    Update OOBEManual.ps1

--- OOBEManual.ps1
+++ OOBEManual.ps1
@@ -1,2 +1,1 @@
-Start /Wait PowerShell -C Set-ExecutionPolicy Unrestricted; Install-Module OSD -Force ; Import-Module OSD -Force
 Start-OSDPad -RepoOwner MyOLC -RepoName MyScripts -BrandingTitle 'Our Learning Cloud - Bhavin Patel' -OAuth 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 -hide script -BrandingColor Magenta
