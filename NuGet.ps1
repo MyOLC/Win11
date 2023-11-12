62de04cc4ea4371196d7b84a7cc64171b37bae42
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 18:55:31 +0000

    Update NuGet.ps1

--- NuGet.ps1
+++ NuGet.ps1
@@ -13,14 +13,14 @@ function Step-oobePackageManagement {
     [CmdletBinding()]
     param ()
     if ($env:UserName -eq 'defaultuser0') {
-        if (Get-Module -Name PowerShellGet -ListAvailable | Where-Object {$_.Version -ge '2.2.5'}) {
-            Write-Host -ForegroundColor Cyan 'PowerShellGet 2.2.5 or greater is installed'
+        if (Get-PackageProvider -Name NuGet -ListAvailable | Where-Object {$_.Version -ge '2.8.5.201'}) {
+            Write-Host -ForegroundColor Cyan 'NuGet 2.8.5.201 or greater is installed'
         }
         else {
             Write-Host -ForegroundColor Cyan 'Install-Package PackageManagement,PowerShellGet'
-            Install-Package -Name PowerShellGet -MinimumVersion 2.2.5 -Force -Confirm:$false -Source PSGallery | Out-Null
+            Install-Package -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false -Source PSGallery | Out-Null
     
-            Write-Host -ForegroundColor Cyan 'Import-Module PackageManagement,PowerShellGet'
+            Write-Host -ForegroundColor Cyan 'Import-Module NuGet'
             Import-Module PackageManagement,PowerShellGet -Force
         }
     }
