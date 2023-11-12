cb60d589c93740674916234980e937f8456e502e
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 10:11:55 +0000

    Update OOBETasks.ps1

--- OOBETasks.ps1
+++ OOBETasks.ps1
@@ -12,8 +12,8 @@ $OOBEScript =@"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
-Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
-Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
+Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
 
 Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
@@ -42,7 +42,7 @@ Unregister-ScheduledTask -TaskName "Scheduled Task for SendKeys" -Confirm:`$fals
 Unregister-ScheduledTask -TaskName "Scheduled Task for OSDCloud post installation" -Confirm:`$false
 
 Write-Host -ForegroundColor DarkGray "Restarting Computer"
-Start-Process PowerShell -ArgumentList "-NoL -C Restart-Computer -Force -Wait
+Start-Process PowerShell -ArgumentList "-NoL -C Restart-Computer -Force" -Wait
 
 Stop-Transcript -Verbose | Out-File
 "@
