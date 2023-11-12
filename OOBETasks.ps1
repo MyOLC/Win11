8e090b0332bea826505b584bf7410727571cb849
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 18:38:43 +0000

    Update OOBETasks.ps1

--- OOBETasks.ps1
+++ OOBETasks.ps1
@@ -13,7 +13,7 @@ $OOBEScript =@"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 Write-Host -ForegroundColor DarkGray "Installing NuGet PS Module"
-Start-Process PowerShell -ArgumentList "-NoLogo -Command {Set-ExecutionPolicy RemoteSigned -Force; Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force; Import-PackageProvider -Name NuGet -Force}" -Wait
+Start-Process PowerShell -ArgumentList "-NoLogo -ExecutionPolicy Bypass -Command {Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser -Verbose; Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser -Verbose;}" -Wait
 
 Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
