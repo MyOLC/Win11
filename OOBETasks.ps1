d4ef399acaa1c7ebe5445ce2c7170236d9e86165
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 12:22:49 +0000

    Update OOBETasks.ps1

--- OOBETasks.ps1
+++ OOBETasks.ps1
@@ -22,7 +22,7 @@ Write-Host -ForegroundColor DarkGray "Executing Autopilot Check Script"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/MyOLC/OSDCloud/Main/check-autopilotprereq.ps1" -Wait
 
 Write-Host -ForegroundColor DarkGray "Running OOBE"
-Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/MyOLC/OSDCloud/Main/check-autopilotprereq.ps1" -Wait
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/MyOLC/Project/main/OOBEDeployV1.ps1" -Wait
 
 Write-Host -ForegroundColor DarkGray "Executing Cleanup Script"
 Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/MyOLC/Project/main/OOBEDeployV1.ps1" -Wait
