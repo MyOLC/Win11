8ff1141471f816b5a09e0dd2bf095e06e1a5b77d
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 10:33:52 +0000

    Update OOBETasks.ps1

--- OOBETasks.ps1
+++ OOBETasks.ps1
@@ -13,6 +13,7 @@ $OOBEScript =@"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
 Write-Host -ForegroundColor DarkGray "Installing NuGet PS Module"
+Start-Process PowerShell -ArgumentList "-NoLogo -Command Set-ExecutionPolicy RemoteSigned -Force"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser
 Start-Process PowerShell -ArgumentList "-NoL -C Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
 
