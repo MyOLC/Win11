1133c1791a2f8bc9f169590abc61d7dfbfd26b13
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2023-11-12 20:20:59 +0000

    Update OOBETasks.ps1

--- OOBETasks.ps1
+++ OOBETasks.ps1
@@ -12,9 +12,6 @@ $OOBEScript =@"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
-Write-Host -ForegroundColor DarkGray "Installing NuGet PS Module"
-Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/MyOLC/Win11/main/NuGet.ps1" -Wait
-
 Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
 
